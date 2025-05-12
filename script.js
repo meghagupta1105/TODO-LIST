ac8af738f1d1a42b24e22313f7960ab2ac505f89
dev_megha <megha@gmail.com>
2025-05-12 13:24:04 +0530

    code added

---
+++ script.js
@@ -0,0 +1,44 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+
+
+document.addEventListener("DOMContentLoaded", showTasks);
+
+
+addTaskBtn.addEventListener("click", () => {
+  const taskText = taskInput.value.trim();
+  if (taskText !== "") {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.push(taskText);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    taskInput.value = "";
+    showTasks();
+  }
+});
+
+
+function showTasks() {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  taskList.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.textContent = task;
+
+    const delBtn = document.createElement("span");
+    delBtn.textContent = "Delete";
+    delBtn.classList.add("delete-btn");
+    delBtn.onclick = () => deleteTask(index);
+
+    li.appendChild(delBtn);
+    taskList.appendChild(li);
+  });
+}
+
+function deleteTask(index) {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.splice(index, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  showTasks();
+}
